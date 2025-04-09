8fd97b05c6a29b4b60add4f2bbc666a0fef9d9c8
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-09 20:13:16 +0600

    added new navbar and footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Footer = () => {
   return (
     <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10 mt-1">
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -11,8 +11,8 @@ const Home = () => {
         }}
       >
         <div className="hero-overlay"></div>
-        <div className="hero-content text-neutral-content text-center">
-          <div className="max-w-md flex flex-col items-start">
+        <div className="hero-content text-neutral-content text-center flex justify-between items-center">
+          <div className="max-w-md flex flex-col items-start mr-[600px]">
             <h1 className="mb-5 text-3xl font-bold text-left">
               Bid in Unique Items from <br />
               Around the World.
@@ -30,11 +30,28 @@ const Home = () => {
         </div>
       </div>
       {/*  */}
-      <div>
+      <div className="m-10 ">
         <h1 className="font-bold my-3">Active Auctions</h1>
         <p className="text-gray-400 mb-5">
           Discover and bid on extraordinary items.
         </p>
+        <div className="grid grid-cols-6 gap-3">
+          <div className="col-span-4 ">
+            <div className="flex justify-between p-5 pr-10 border-2 rounded-lg">
+              <h5 className="font-bold">Items</h5>
+              <h5 className="font-bold">Current Bid</h5>
+              <h5 className="font-bold">Time Left</h5>
+              <h5 className="font-bold">Bid Now</h5>
+            <div className="divider"></div>
+            </div>
+          </div>
+          <div className="col-span-2">
+            <h3>laskjfdlaksdjf</h3>
+            <h3>laskjfdlaksdjf</h3>
+            <h3>laskjfdlaksdjf</h3>
+            <h3>laskjfdlaksdjf</h3>
+          </div>
+        </div>
       </div>
     </div>
   );
