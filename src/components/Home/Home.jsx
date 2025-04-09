8da743d0d002bded492c4b5f59aa3f6a863e29ec
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-09 16:22:49 +0600

    added new navbar and footer

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,27 +1,25 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import CoverImg from "../../assets/assets/Banner-min.jpg";
 
 const Home = () => {
   return (
-    <div
-      className="hero min-h-screen mt-2"
-      style={{
-        backgroundImage:
-          "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
-      }}
-    >
-      <div className="hero-overlay"></div>
-      <div className="hero-content text-neutral-content text-center">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Aucion GALLERY</h1>
-          <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="flex flex-row justify-around items-center gap-40 lg:flex-row-reverse  ">
+        <div>
+          <img src={CoverImg} className="max-w-sm rounded-lg shadow-2xl" />
+        </div>
+        <div>
+          <h1 className="text-5xl font-bold">
+            <span className="text-red-700">Auction </span>Gallery <br />{" "}
+            <span className="ml-28 text-green-700">Occation</span> !
+          </h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. <br /> Quaerat fugiat ut
+            assumenda
           </p>
-          <Link to='/details'>
+          <Link to="/details">
             <button className="btn btn-primary">View Details</button>
-          </Link>
+          </Link>{" "}
         </div>
       </div>
     </div>
